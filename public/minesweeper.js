b4fe5aff541e3b2846a177c86039bbd7b02ddf96
flynger <arnavsingh2516@gmail.com>
2022-10-18 21:46:37 -0700

    changes

--- public/minesweeper.js
+++ public/minesweeper.js
@@ -68,8 +68,6 @@ function sendEvent(name) {
 
 client.init("localhost:3000");
 
-var mouseHeldDown = false;
-
 class Minesweeper {
     constructor() {
         this.TILE_SIZE = 32,
@@ -101,7 +99,7 @@ class Minesweeper {
         for (let i = 0; i < this.settings.height; i++) {
             grid += this.createImg("border-v");
             for (let j = 0; j < this.settings.width; j++) {
-                grid += this.createImg("square blank", i + "_" + j);
+                grid += this.createImg("blank", i + "_" + j);
             }
             grid += this.createImg("border-v");
             grid += "<br>";
@@ -120,32 +118,42 @@ class Minesweeper {
         console.log($("#game").data("game"));
 
         // create mouse events
-        $("#game").on("mouseup", { game: $("#game").data("game") }, function (e) {
-            if (e.button == 0) {
-                let game = e.data.game;
-                mouseHeldDown = false;
-                e.preventDefault();
-                if ($(e.target).hasClass("empty")) {
-                    let id = $(e.target).attr("id").split("_");
-                    let x = +id[1];
-                    let y = +id[0];
-                    if (game.GRID.length == 0) {
-                        game.GRID = game.createBoard(x, y, game.settings.width, game.settings.height, game.settings.mines);
+        $("#game").on("mouseup", { game: $("#game").data("game") }, e => {
+            e.preventDefault();
+            let game = e.data.game;
+            let [x, y] = this.getCellFromID($(e.target).attr("id"));
+            switch (e.which) {
+                case 1:
+                    if ($(e.target).hasClass("empty")) {
+                        if (game.GRID.length == 0) {
+                            game.GRID = game.createBoard(x, y, game.settings.width, game.settings.height, game.settings.mines);
+                        }
+                        game.clearCell(x, y);
                     }
-                    game.clearSquare(x, y);
-                }
-
+                    break;
+                case 2:
+                    //alert('Middle mouse button is pressed');
+                    if (game.satisfyFlags(x, y)) {
+                        game.clearCells(x, y);
+                    }
+                    break;
+                case 3:
+                    // clear cells around mouse
+                    // if (game.satisfyFlags(x, y)) {
+                    //     game.clearCells(x, y);
+                    // }
+                    break;
+                default:
+                    alert('Nothing');
             }
         });
 
-        $("#game").unbind("mousedown").on("mousedown", e => {
-            console.log(e);
+        $("#game").unbind("mousedown").on("mousedown", { game: $("#game").data("game") }, e => {
             e.preventDefault();
             switch (e.which) {
                 case 1:
-                    mouseHeldDown = true;
                     if ($(e.target).hasClass("blank")) {
-                        $(e.target).attr("class", "square empty");
+                        $(e.target).attr("class", "empty");
                     }
                     break;
                 case 2:
@@ -153,13 +161,14 @@ class Minesweeper {
                     break;
                 case 3:
                     if ($(e.target).hasClass("blank")) {
-                        // if square blank, add flag
-                        $(e.target).attr("class", "square bombflagged");
+                        // if cell blank, add flag
+                        $(e.target).attr("class", "bombflagged");
                     } else if ($(e.target).hasClass("bombflagged")) {
                         // if flag, revert to blank
-                        $(e.target).attr("class", "square blank");
-                    } else if ($(e.target).hasClass("empty")) {
-                        // if left click is on, clear squares
+                        $(e.target).attr("class", "blank");
+                    } else if (e.which == 1) {
+                        // if left click is on, clear cells
+                        e.data.game.clearCells(x, y);
                     }
                     break;
                 default:
@@ -167,17 +176,25 @@ class Minesweeper {
             }
         });
 
-        $("#game").on("mouseout", function (e) {
+        $("#game").on("mouseout", e => {
             e.preventDefault();
             if ($(e.target).hasClass("empty")) {
-                $(e.target).attr("class", "square blank");
+                $(e.target).attr("class", "blank");
             }
         });
 
-        $("#game").on("mouseover", function (e) {
+        $("#game").on("mouseover", { game: $("#game").data("game") }, e => {
             e.preventDefault();
-            if (mouseHeldDown && $(e.target).hasClass("blank")) {
-                $(e.target).attr("class", "square empty");
+            switch (e.buttons) {
+                case 1:
+                    let [x, y] = this.getCellFromID($(e.target).attr("id"));
+                    e.data.game.selectCell(x, y);
+                    break;
+                case 3:
+                    //selectCell(x, y);
+                    break;
+                default:
+                // nothing
             }
         });
     }
@@ -219,6 +236,7 @@ class Minesweeper {
         console.log(grid);
         return grid;
     }
+    // count mines while generating
     countMines(grid, x, y) {
         let mines = 0;
         for (let v = -1; v <= 1; v++) {
@@ -228,18 +246,15 @@ class Minesweeper {
         }
         return mines;
     }
-    countFlags(x, y) {
-        for (let v = -1; v <= 1; v++) {
-            for (let h = -1; h <= 1; h++) {
-                let thisX = x + h;
-                let thisY = y + v;
-                let flags = 0;
-                if (this.GRID[thisY] && this.GRID[thisY][thisX] != undefined && !(h == 0 && v == 0) && this.getCanvasCell(thisX, thisY).hasClass("bombflagged")) flags++;
-            }
-        }
-        return flags;
+    // count flags in order to clear area
+    satisfyFlags(x, y) {
+        var flags = 0;
+        this.doCellOperation(x, y, (thisX, thisY, thisCell) => {
+            if (thisCell.hasClass("bombflagged")) flags++;
+        });
+        return this.getCanvasCell(x, y).attr('class').includes("open") && flags === this.GRID[y][x];
     }
-    clearSquare(x, y) {
+    clearCell(x, y) {
         let cell = this.GRID[y][x];
         let classToAdd;
         // death check
@@ -249,37 +264,75 @@ class Minesweeper {
                     var isFlagged = this.getCanvasCell(col, row).hasClass("bombflagged");
                     if (this.GRID[row][col] === "X") {
                         if (!isFlagged) {
-                            this.getCanvasCell(col, row).attr("class", "square bombrevealed");
+                            this.getCanvasCell(col, row).attr("class", "bombrevealed");
                         }
                     }
                     else if (isFlagged) {
-                        this.getCanvasCell(col, row).attr("class", "square bombmisflagged");
+                        this.getCanvasCell(col, row).attr("class", "bombmisflagged");
                     }
                 }
             }
-            $("#game").off("mousedown");
+            $("#game").off();
             classToAdd = "bombdeath";
         } else {
             classToAdd = "open" + cell;
         }
-        // open the square
-        this.getCanvasCell(x, y).attr("class", "square " + classToAdd);
-        // if a 0, open nearby squares
+        // open the cell
+        this.getCanvasCell(x, y).attr("class", classToAdd);
+        // if a 0, open nearby cells
         if (cell === 0) {
-            this.clearSquares(x, y);
+            this.clearCells(x, y);
         }
     }
-    clearSquares(x, y) {
-        for (let v = -1; v <= 1; v++) {
-            for (let h = -1; h <= 1; h++) {
-                let thisX = x + h;
-                let thisY = y + v;
-                if (this.GRID[thisY] && this.GRID[thisY][thisX] != undefined && !(h == 0 && v == 0) && this.getCanvasCell(thisX, thisY).hasClass("blank")) this.clearSquare(thisX, thisY);
-            }
-        }
+    clearCells(x, y) {
+        this.doCellOperation(x, y, (thisX, thisY, thisCell) => {
+            if (thisCell.hasClass("blank")) this.clearCell(thisX, thisY);
+        });
+        // for (let v = -1; v <= 1; v++) {
+        //     for (let h = -1; h <= 1; h++) {
+        //         let thisX = x + h;
+        //         let thisY = y + v;
+        //         if (this.getCanvasCell(thisX, thisY).length && this.getCanvasCell(thisX, thisY).hasClass("blank")) this.clearCell(thisX, thisY);
+        //     }
+        // }
     }
     getCanvasCell(x, y) {
         return $("#" + y + "_" + x);
+    }
+    getCellFromID(id) {
+        let [cellY, cellX] = id.split("_");
+        // return x, y of cell
+        return [+cellX, +cellY];
+    }
+    selectCell(x, y) {
+        let cell = this.getCanvasCell(x, y);
+        // if cell exists and is blank
+        if (cell.length && cell.hasClass("blank")) {
+            cell.attr("class", "empty");
+        }
+    }
+    selectCells(x, y) {
+        // if cell exists and is blank
+        for (let v = -1; v <= 1; v++) {
+            for (let h = -1; h <= 1; h++) {
+                let cell = this.getCanvasCell(x + h, y + v);
+                if (cell.length && cell.hasClass("blank")) this.selectCell(x + h, y + v);
+            }
+        }
+    }
+    doCellOperation(x, y, func) {
+        for (let ny = y - 1; ny <= y + 1; ny++) {
+            for (let nx = x - 1; nx <= x + 1; nx++) {
+                let cell = this.getCanvasCell(nx, ny);
+                if (cell.length) func(nx, ny, cell);
+            }
+        }
+    }
+    deselectAllCells() {
+        // let cell = this.getCanvasCell(x, y);
+        // if ($(cell).hasClass("blank")) {
+        //     $(cell).attr("class", "empty");
+        // }
     }
 }
 
