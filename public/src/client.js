e2542a4d37209e561026e99be68ac75eac7bb00f
flynger <arnavsingh2516@gmail.com>
2022-11-19 18:09:09 -0800

    separated socket stuff from minesweeper code and moved files into src
    socket stuff --> client
    minesweeper stuff --> main
    chat stuff --> chat
    all js files in src/*.js

---
+++ public/src/client.js
@@ -0,0 +1,25 @@
+var latency = -1;
+var link = "73.109.23.105:3000"; //73.109.23.105
+var socket = io.connect(link);
+
+setInterval(() => {
+    const start = Date.now();
+
+    socket.emit("ping", () => {
+        const duration = Date.now() - start;
+        latency = duration;
+    });
+}, 1000);
+
+socket.on("connect", (ms) => {
+    // connect event
+});
+socket.on("pong", (ms) => {
+    latency = ms;
+});
+socket.on("chatMessage", (data) => {
+    if (data.user == "Server") {
+        addServerMessage(data.msg);
+    }
+    else addChatMessage(data.user, data.msg);
+});
