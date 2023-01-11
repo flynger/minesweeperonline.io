29fff3a7d5d151d7d9af7b613e0321d87e7f68ca
flynger <arnavsingh2516@gmail.com>
2023-01-11 15:38:47 -0800

    client update w/ new server flags

--- public/src/client.js
+++ public/src/client.js
@@ -59,13 +59,7 @@ socket.on("boardData", (data) => {
                 let classToAdd = "";
                 let value =  data.board[row][col];
                 if (minesweeper.GRID[row][col] === "F") {
-                    if (data.gameOver) {
-                        value = value === "X" ? "?" : "FX";
-                    } else {
-                        if (value !== "?") {
-                            minesweeper.FLAGS++;
-                        } else value = "?";
-                    }
+                    minesweeper.FLAGS++;
                 }
                 switch (value) {
                     case "?":
@@ -79,7 +73,7 @@ socket.on("boardData", (data) => {
                         break
                     case "X":
                         // bomb (gameover)
-                        classToAdd = "bombrevealed";
+                        classToAdd = "bombrevealed"; 
                         break
                     case "FX":
                         classToAdd = "bombmisflagged";
